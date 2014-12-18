f1a1e5e64d73ec23f36da7a8462a7b3a483cccbd
Lionel Lecaque <lionel@taotesting.com>
2014-12-18 11:54:30 +0000

    add install for c9

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+php tao/scripts/taoInstall.php --db_driver pdo_mysql --db_name c9 --db_user llecaque --db_host 0.0.0.0 --module_host http://package-tao.llecaque.c9.io --module_name tao30 --module_namespace http://trunk.localdomain/tao.rdf --module_url http://package-tao.llecaque.c9.io --user_login admin --user_pass admin
